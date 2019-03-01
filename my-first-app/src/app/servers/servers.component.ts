98c98155000c0822081b8703a2f692137e748513
Akul Aggarwal <akul@avinetworks.com>
2019-02-28 18:34:08 -0800

    Data binding practice and Add assignment 2

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreateService = 'No Server Was Created';
+  serverName = '';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreateService = `Server was Created. Server Name is ${this.serverName}`;
+  }
+
+  onUpdateServerName(ev: any) {
+    this.serverName = ev.target.value;
   }
 
 }
